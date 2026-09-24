a045576206bef7cb40cbd2083e4cf3c6673f5291
Luciano Rossini <lucianorossini_34@hotmail.com>
2026-09-23 23:55:35 -0300

    ejercicio 2

---
+++ Trabajos de Alumnos/LucianoRossini-67065/ejercicio2/ejercicio2/Program.cs
@@ -0,0 +1,40 @@
+﻿float[] timeRun = new float [5];
+for (int i = 0; i < timeRun.Length; i++)
+{
+    Console.WriteLine ("Ingrese timepo de participante");
+    timeRun[i] = float.Parse(Console.ReadLine());
+}
+
+bool continuar = true;
+
+while (continuar)
+{
+    Console.WriteLine("Ingrese la marca objetivo:");
+    float marcas = float.Parse(Console.ReadLine());
+
+    int contador = 0;
+
+    for (int i = 0; i < timeRun.Length; i++) 
+    {
+        if (timeRun[i] <= marcas)
+        {
+            contador++;  
+
+        }
+
+    }
+    Console.WriteLine ("corredores que superaron la prueba: " + contador);
+    Console.WriteLine("Deseas seguir?");
+    string respuesta = Console.ReadLine();
+    if (respuesta == "n")
+    {
+        continuar = false;
+    }
+    else 
+    { 
+        continuar = true;
+    }
+
+}
+
+
