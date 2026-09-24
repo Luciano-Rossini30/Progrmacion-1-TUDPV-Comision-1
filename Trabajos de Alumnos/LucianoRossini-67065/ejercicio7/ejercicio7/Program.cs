79639769c86f98e45eefb2d62af016ed16b33522
Luciano Rossini <lucianorossini_34@hotmail.com>
2026-09-24 17:48:38 -0300

    ejercicio7

---
+++ Trabajos de Alumnos/LucianoRossini-67065/ejercicio7/ejercicio7/Program.cs
@@ -0,0 +1,20 @@
+﻿bool[] trampas = {false, true, false, false, true, false };
+int casillero = 0;
+
+while (!trampas[casillero] && casillero != 5)
+{
+    casillero = casillero + 1;
+    if (trampas[casillero] == true)
+    {
+        Console.WriteLine("Perdiste, pisaste una trampa");  
+    }
+    else
+    {
+        if (casillero == 5)
+        {
+            Console.WriteLine("se completo el recorrido");
+        }
+    }
+}
+
+//me re costo el ejercicio este porque no entendia si hice mal pero en realidad la consigna pide que al primer error se cancele el while y pierda pero en el vector nro 1 ya es trampa, al no llegar al final o tener mas opciones la dude toda
