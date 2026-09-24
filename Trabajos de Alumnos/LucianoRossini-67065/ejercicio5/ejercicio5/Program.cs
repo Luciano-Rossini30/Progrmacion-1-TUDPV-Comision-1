66c266a42c14ff0708f3ad7d9ee382169b960543
Luciano Rossini <lucianorossini_34@hotmail.com>
2026-09-24 08:10:10 -0300

    ejercicio5

---
+++ Trabajos de Alumnos/LucianoRossini-67065/ejercicio5/ejercicio5/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] flechas = new int[6];
+for (int i = 0; i < flechas.Length; i++)
+{
+    Console.WriteLine("ingrese damage de flechas");
+    flechas[i] = int.Parse(Console.ReadLine());
+}
+
+bool continuar = true;
+
+while (continuar)
+{
+    Console.WriteLine("Damage de referencia?");
+   int referencia = int.Parse(Console.ReadLine());
+
+    int suma = 0;
+
+    for (int i = 0; i < flechas.Length; i++)
+    {
+        if (flechas[i] > referencia)
+        {
+            suma += flechas[i];
+        }
+        else
+        {
+            Console.WriteLine("Impacto descartado");
+        }
+    }
+    Console.WriteLine("la suma del damage es de " + suma);
+    Console.WriteLine("Desea seguir o salir?");
+    string respuesta = Console.ReadLine();
+   
+    if(respuesta == "seguir")
+    {
+     
+    }
+    else { continuar = false; }
+}
