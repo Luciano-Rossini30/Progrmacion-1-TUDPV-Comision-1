6834335e02ab65c5f00e1da8f62d22e638388850
Luciano Rossini <lucianorossini_34@hotmail.com>
2026-09-24 21:07:16 -0300

    ejercicio10

---
+++ Trabajos de Alumnos/LucianoRossini-67065/ejercicio10/ejercicio10/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] experiencia = new int[5];
+
+for (int i = 0; i < experiencia.Length; i++)
+{
+    Console.WriteLine("Ingrese la EXP obtenida en la misión " + (i + 1));
+    experiencia[i] = int.Parse(Console.ReadLine());
+
+    if (experiencia[i] > 100)
+    {
+        experiencia[i] = experiencia[i] + (experiencia[i] * 20 / 100);
+    }
+    else
+    {
+        Console.WriteLine("La misión no recibe bonificación.");
+    }
+}
+
+int total = 0;
+
+Console.WriteLine("Tabla de experiencia actualizada:");
+
+for (int i = 0; i < experiencia.Length; i++)
+{
+    Console.WriteLine("Misión " + (i + 1) + ": " + experiencia[i] + " EXP");
+    total = total + experiencia[i];
+}
+
+Console.WriteLine("Experiencia total acumulada: " + total);
